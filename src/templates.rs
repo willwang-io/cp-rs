137a359206e6cf7ccc122fd591323def505689f3
willwang-io <brill.13.koi@icloud.com>
2025-07-23 22:08:41 -0600

    Add a basic Rust template to read from stdin

---
+++ src/templates.rs
@@ -0,0 +1,32 @@
+use std::io::Read;
+
+fn solve(sc: &mut Scanner) -> () {
+
+}
+
+fn main() {
+    let mut sc = Scanner::new();
+    let t: usize = sc.next();
+    for _ in 0..t {
+        solve(&mut sc);
+    }
+}
+
+struct Scanner {
+    iter: std::str::SplitWhitespace<'static>,
+}
+
+impl Scanner {
+    fn new() -> Self {
+        let mut tmp = String::new();
+        std::io::stdin().read_to_string(&mut tmp).unwrap();
+        let s: &'static str = Box::leak(tmp.into_boxed_str());
+        Scanner {
+            iter: s.split_whitespace(),
+        }
+    }
+
+    fn next<T: std::str::FromStr>(&mut self) -> T {
+        self.iter.next().unwrap().parse().ok().expect("parse error")
+    }
+}
